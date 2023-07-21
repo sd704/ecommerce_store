3c665f5f0608bb3d707b10a2d4955e1899556240
Sagar Dutta <sagar.dutta.24.1@mountblue.tech>
2023-07-21 15:58:32 +0530

    Added debouncing to search, added association to user and item

--- app/javascript/home_clicks.js
+++ app/javascript/home_clicks.js
@@ -4,50 +4,70 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = 'search.html';
     });
 
-    document.getElementById("search").addEventListener("click", () => {
+    const allItems = document.getElementById("items_container").innerHTML
+
+    function searchRequest() {
         const itemsContainer = document.getElementById("items_container")
         const query = document.getElementById("query").value
 
-        const itemData = {
-            name: query,
-            brand: query,
-            category: query,
-        }
+        if (query.length >= 3) {
 
-        const params = new URLSearchParams(itemData);
-        const url = `/customsearch?${params}`;
-
-        fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
+            const itemData = {
+                name: query,
+                brand: query,
+                category: query,
             }
-        }).then(response => response.json())
-            .then(data => {
-                itemsContainer.innerHTML = ""
 
-                if (data.list.length < 1) {
-                    itemsContainer.innerHTML = "Sorry, no products found."
+            const params = new URLSearchParams(itemData);
+            const url = `/customsearch?${params}`;
+
+            fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
                 }
+            }).then(response => response.json())
+                .then(data => {
+                    itemsContainer.innerHTML = ""
 
-                data.list.map((product) => {
-                    itemsContainer.innerHTML += `
-                    <div class="item_container" onclick="window.location.href='/product/${product.id}'">
-                    <div class="product_image" style="background: url('assets/shoes.avif');"></div>
-                    <div class="info_container">
+                    if (data.list.length < 1) {
+                        itemsContainer.innerHTML = "Sorry, no products found."
+                    }
+
+                    data.list.map((product) => {
+                        itemsContainer.innerHTML += `
+                        <div class="item_container" onclick="window.location.href='/product/${product.id}'">
+                        <div class="product_image" style="background: url('assets/shoes.avif');"></div>
+                        <div class="info_container">
                         <p class="product_name">${product.brand} ${product.name}</p>
                         <p class="product_price">$${product.price}</p>
                         <div class="ratings"></div>
-                    </div>
-                    </div>
-                    `
-                })
+                        </div>
+                        </div>
+                        `
+                    })
 
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    });
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        } else {
+            itemsContainer.innerHTML = allItems
+        }
+    }
+
+    function deBounce(searchRequest, delay) {
+        let debounceTimer;
+        return function () {
+            if (debounceTimer) {
+                clearTimeout(debounceTimer);
+            }
+            debounceTimer = setTimeout(() => { searchRequest(); }, delay);
+        }
+    }
+
+    document.getElementById("query").addEventListener("input", deBounce(searchRequest, 500));
+    document.getElementById("search").addEventListener("click", searchRequest());
 
     document.getElementById("cart").addEventListener("click", () => {
         window.location.href = 'cart.html';
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
+    # belongs_to :user, foreign_key: 'user_id', class_name: 'User'
+    belongs_to :user
     has_many :cart_items
 end
